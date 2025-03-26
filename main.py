4d1d47b0e7450a2879f4e4ff192ffc491b676440
Nikita Serdiuk <nihttoter@gmail.com>
2025-03-26 11:35:17 +0000

    Move code to normalize function.

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import matplotlib
 
 from utils.normalize_dataset import normalize_dataset
 
-original_ds = pd.read_json("./data/raw.json")
-normalized_ds = original_ds.apply(normalize_dataset, axis="columns")
-normalized_ds = normalized_ds.drop('month', axis="columns")
-normalized_ds = normalized_ds.drop('year', axis="columns")
-normalized_ds = normalized_ds.drop('day', axis="columns")
-normalized_ds = normalized_ds.drop('steps', axis="columns")
+normalized_dataset = normalize_dataset(path="./data/raw.json", output_path="./data/raw-normalized.json")
 
-normalized_ds.to_json('./data/raw-normalized.json', orient='records')
-
-
-print(normalized_ds.head(1))
+print(normalized_dataset.head(1))
--- utils/normalize_dataset.py
+++ utils/normalize_dataset.py
@@ -1,7 +1,8 @@
 import datetime
+import pandas
 
 
-def normalize_dataset(row):
+def normalize_dataset_row(row):
     # row["ApplicationSubmitted"] = datetime.date(year=row.year, day=row.day, month=row.month + 1)
     index = 0
     row.steps[6]["duration"] = 0
@@ -18,5 +19,17 @@ def normalize_dataset(row):
                     row[step["stepId"]] = row[row.steps[index - 1]["stepId"]] + datetime.timedelta(
                         days=step["duration"])
         index += 1
-    print(row)
     return row
+
+
+def normalize_dataset(path="./data/raw.json", output_path="./data/raw-normalized.json"):
+    # "./data/raw.json"
+    # "./data/raw-normalized.json"
+    original_ds = pandas.read_json(path)
+    normalized_ds = original_ds.apply(normalize_dataset_row, axis="columns")
+    normalized_ds = normalized_ds.drop('month', axis="columns")
+    normalized_ds = normalized_ds.drop('year', axis="columns")
+    normalized_ds = normalized_ds.drop('day', axis="columns")
+    normalized_ds = normalized_ds.drop('steps', axis="columns")
+    normalized_ds.to_json(output_path, orient='records')
+    return pandas.read_json(output_path)
