3d8d4a73418ab4c6db34e69c76f61431115a50c5
Nikita Serdiuk <nihttoter@gmail.com>
2025-03-27 11:40:21 +0000

    Move code to normalize function.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as pyplot
+import datetime
 
 from utils.normalize_dataset import normalize_dataset
 
 normalized_dataset = normalize_dataset(path="./data/raw.json", output_path="./data/raw-normalized.json")
+
+submit_set = normalized_dataset["ApplicationSubmitted"].map(
+    lambda date: datetime.datetime.fromtimestamp(date))
+pyplot.hist(submit_set)
+pyplot.show()
 
 print(normalized_dataset.head(1))
